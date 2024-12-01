06352c026373a004c444745e11db32017f40aab4
stoppedwumm <mobileboxnews@icloud.com>
2024-12-01 08:28:05 +0100

    add terminal restart

--- __main__.py
+++ __main__.py
@@ -23,6 +23,8 @@ print("Installing XCode and Homebrew, please accept any licenses, warnings etc..
 exec("xcode-select --install")
 if not hbFolderExists:
     exec('/bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"')
+    print("Installed Homebrew! Please restart your terminal...")
+    exit(0)
 
 exec("brew install git")
 exec("brew install sdl2 freetype2 fontconfig pkg-config opus libpng libedit")
